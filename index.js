7458fe3f5411f805912e698f67cab54e1fbea1c7
Alejandro <alejandrobrb210501@gmail.com>
2022-10-05 23:02:00 +0200

    Add routes.js file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express'); 
+const router = require('./src/routes/routes.js');
+
 const app = express();       
 const port = 5000;
 
-app.get('/', (req, res) => { 
-    res.end('Hello world !');
-});
+app.use(express.urlencoded({extended:true}));
+
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
-}); 
+});
